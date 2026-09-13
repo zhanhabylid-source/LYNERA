b295aa44607a24b3508c879d0cbf004e5f2c45c3
zhanhabylid-source <zhanhabyl.id@gmail.com>
2026-09-13 17:05:41 +0700

    Use shared favicon on public booking

--- resources/views/public-booking/form.blade.php
+++ resources/views/public-booking/form.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+        <link rel="icon" type="image/png" href="{{ asset('images/logo/pavicon.png') }}">
+        <link rel="shortcut icon" type="image/png" href="{{ asset('images/logo/pavicon.png') }}">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
